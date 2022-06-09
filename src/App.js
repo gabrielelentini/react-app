5c3b3599c5b2fb8b65b43c79b4b46dcc46fd6c1d
Gabriele Lentini <gabriele.lentini@keyoneweb.it>
2022-06-09 17:55:55 +0200

    inserito componente square

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const squareClass = "yellow";
+
+  const squares = new Array();
+
+  for (let i = 0; i <= 15; i++) {
+    squares.push({
+      id: i
+    });
+  };
+
+  const Square = (props) => {
+    return <div className={props.colorClass}>{props.index}</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flexbox">
+          { squares.map(s => <Square colorClass={squareClass} index={s.id} key={s.id} />) }
     </div>
+
   );
 }
 
