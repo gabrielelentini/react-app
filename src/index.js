1d69aa60a47e0793cf36265e3c157f232ae7394a
Gabriele Lentini <gabriele.lentini@keyoneweb.it>
2022-06-10 17:36:18 +0200

    RollOfTheDice counters

--- src/index.js
+++ src/index.js
@@ -4,20 +4,22 @@ import Header  from './components/Header';
 import Modal  from './components/Modal';
 import ListaSpesa from './components/ListaSpesa'
 import './style/style.css'
+import RollOfTheDice from './components/RollOfTheDice';
 
 function App(){
-  const [listaSpesa, setListaSpesa] = useState([
-    {fatto: false, titolo:'Pane'},
-    {fatto: true, titolo:'Uova'},
-    {fatto: false, titolo:'Latte'},
-  ]);
-  const [modalVisible, setModalVisible] = useState(false)
-  let mux = <Modal setListaSpesa={setListaSpesa} listaSpesa={listaSpesa} setModalVisible={setModalVisible} />
+  // const [listaSpesa, setListaSpesa] = useState([
+  //   {fatto: false, titolo:'Pane'},
+  //   {fatto: true, titolo:'Uova'},
+  //   {fatto: false, titolo:'Latte'},
+  // ]);
+  // const [modalVisible, setModalVisible] = useState(false)
+  // let mux = <Modal setListaSpesa={setListaSpesa} listaSpesa={listaSpesa} setModalVisible={setModalVisible} />
   return (
     <div className="App">
-      <Header setModalVisible={setModalVisible} modalVisible={modalVisible}/>
+      {/* <Header setModalVisible={setModalVisible} modalVisible={modalVisible}/>
       { modalVisible ? mux : false }
-      <ListaSpesa listaSpesa={listaSpesa} setListaSpesa={setListaSpesa} /> 
+      <ListaSpesa listaSpesa={listaSpesa} setListaSpesa={setListaSpesa} />  */}
+      <RollOfTheDice />
     </div>
   )
 }
